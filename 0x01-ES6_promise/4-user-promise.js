ad852dcb1c1afa53cc5935823a0d7a9eabeff246
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-04-20 14:08:58 +0200

    Update 4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,11 +1,8 @@
-// sign up user
-function signUpUser(firstName, lastName) {
-	return Promise.resolve(
-		{
-			firstName,
-			lastName,
-		},
-	);
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
 }
-
-export default signUpUser;
