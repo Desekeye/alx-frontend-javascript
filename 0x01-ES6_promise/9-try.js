42cb0fe79394d38d7440d7bba3faaa9bf37ff32a
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-04-20 14:14:38 +0200

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,18 +1,14 @@
 // Queue push
-function guardrail(mathFunction) {
-	const queue = [];
-	let result;
-	
-	try {
-		result = mathFunction();
-	} catch (e) {
-		result = e.toString();
-	}
-	
-	queue.push(result);
-	queue.push('Guardrail was processed');
+export default function guardrail(mathFunction) {
+  const queue = [];
 
-	return queue;
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
 }
-
-export default guardrail;
