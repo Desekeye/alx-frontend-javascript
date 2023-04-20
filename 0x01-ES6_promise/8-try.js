9e0c0ba26f9ef594f6a0a84e335a3accbad3b42f
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-04-20 14:13:31 +0200

    Update 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,10 +1,8 @@
 // Divide by 0
 export default function divideFunction(numerator, denominator) {
-	if (denominator === 0) {
-		throw Error('cannot divide by 0');
-	}
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
 
-	const result = numerator / denominator;
-
-	return result;
+  return numerator / denominator;
 }
