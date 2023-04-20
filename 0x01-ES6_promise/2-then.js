4b9b3f176f1fe15c4fd8a18fcc43457c5e116408
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-04-20 14:06:36 +0200

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,10 @@
-// Promise is handle or not
 function handleResponseFromAPI(promise) {
-	return promise
-	.then(() => ({
-		status: 200,
-		ody: 'success',
-	}))
-	.catch(() => Error())
-	.finally(() => console.log('Got a response from the API'));
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
-
 export default handleResponseFromAPI;
