3d32aa92b4d2c6d8efe324373e45ac9f2f692c2d
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-04-20 14:08:05 +0200

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,13 @@
 /* Handle multiple successful promises
  *     Put () because return Promise resolve object
  */
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-	return Promise.all([uploadPhoto(), createUser()])
-	.then((values) => {
-		const { body } = values[0];
-		const { firstName, lastName } = values[1];
-	});
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
