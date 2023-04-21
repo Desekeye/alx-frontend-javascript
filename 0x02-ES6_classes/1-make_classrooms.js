565e471c7396301b45f5590e3b844c7214f9d041
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-04-21 03:11:11 +0200

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
-import ClassRoom from './classroom';
+import ClassRoom from './0-classroom';
 
-export default function iniatializeRoom() {
-	return [
-		new ClassRoom (19),
-		new ClassRoom (20),
-		new classRoom (34),
-	];
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
