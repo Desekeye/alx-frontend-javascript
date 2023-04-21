c55f34672022e37b30b64717d3df11670c8bd4c8
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-04-21 13:22:11 +0200

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -2,21 +2,16 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
+    // call constructor of super class (Building)
     super(brand, motor, color);
-    this.range = range;
+
+    // Create objs
+    this._range = range;
   }
 
-  get range() {
-    return this._range;
-  }
+  // Methods
 
-  set range(value) {
-    this._range = value;
-  }
-
-  cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+  static get [Symbol.species]() {
+    return Car;
   }
 }
